ff770e6e77dd17a1758a3cbcc0db96886c30a80f
NoahDuke <32299907+NoahDuke@users.noreply.github.com>
2017-11-02 08:11:32 -0500

    Update converterlib.h

--- converterlib.h
+++ converterlib.h
@@ -5,3 +5,11 @@ float cm_to_in(void)
     printf("\nThe number of inches is %.2f\n", in);
     return in;
 }
+
+float ft_to_m(void)
+{
+    float ft = get_float("Number of Feet");
+    float m = ft*3.28;
+    printf("The number of meters is %.2f\n", m);
+    return m;
+}
