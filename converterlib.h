b982f1f4fafe686235673cd92d8666b489e83c6e
Zak Wolfgram <31658325+Wolfgzac000@users.noreply.github.com>
2017-11-02 08:10:51 -0500

    Update converterlib.h

--- converterlib.h
+++ converterlib.h
@@ -5,3 +5,11 @@ float cm_to_in(void)
     printf("\nThe number of inches is %.2f\n", in);
     return in;
 }
+
+float in_to_kilometers(void)
+{
+    float in = get_float("Number of inches:");
+    float kilometers = (in * .0000254);
+    printf("\nThe number of kilometers is %.7f\n", kilometers);
+    return kilometers;
+}
