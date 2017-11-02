9bab9654fda8d7787febbf8f2702b8e10c9a5572
BDcodingGavin <31658550+BDcodingGavin@users.noreply.github.com>
2017-11-02 08:10:35 -0500

    Update converterlib.h

--- converterlib.h
+++ converterlib.h
@@ -5,3 +5,11 @@ float cm_to_in(void)
     printf("\nThe number of inches is %.2f\n", in);
     return in;
 }
+
+float in_to_cm(void)
+{
+    float in = get_float("Number of inches:");
+    float cm = in * 2.54;
+    printf("\nThe number of centimeters is %.2f\n",cm);
+    return cm;
+}
